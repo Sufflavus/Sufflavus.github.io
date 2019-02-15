9bfe06c4103b6fd7b2f021e6342dd76163a66f1f
Sufflavus <tatashat@gmail.com>
2019-02-15 21:07:49 +0200

    * styles + slow scroll

--- js/common.js
+++ js/common.js
@@ -1,7 +1,8 @@
-(function () {
+(function ($) {
+    var navHeaderHeight = $("#navHeader")[0].offsetHeight;
     var $aboutSection = document.getElementById("about");
-    var $workNavItem = document.getElementById("workNavItem");
-    var $abautNavItem = document.getElementById("aboutNavItem");
+    var $workNavItem = $("#workNavItem");
+    var $abautNavItem = $("#aboutNavItem");
     var navActiveClassName = "nav__item_active";
 
     document.getElementById("year").textContent=(new Date()).getFullYear();
@@ -9,21 +10,30 @@
     if($aboutSection) {
         onScroll();
         window.addEventListener('scroll', onScroll);
+
+        $(".nav__link").click(goToLink);
+    }
+
+    function goToLink() {
+        var href = $(this).attr("href");
+        var index = href.indexOf("#");
+        var titleId = href.substring(index);
+        var $title = $($(titleId)[0]);
+        $('html,body').animate({ scrollTop: $title.offset().top - navHeaderHeight }, 'slow');
+        return false;
     }
 
 
     function onScroll() {
         var windowHeight = document.documentElement.clientHeight;
-        var scrollY = window.scrollY;
-        
         var aboutTopPosition = $aboutSection.getBoundingClientRect().top;
 
         if(aboutTopPosition < windowHeight/3) {
-            $workNavItem.classList.remove(navActiveClassName);
-            $abautNavItem.classList.add(navActiveClassName);
+            $workNavItem.removeClass(navActiveClassName);
+            $abautNavItem.addClass(navActiveClassName);
         } else {
-            $workNavItem.classList.add(navActiveClassName);
-            $abautNavItem.classList.remove(navActiveClassName);
+            $workNavItem.addClass(navActiveClassName);
+            $abautNavItem.removeClass(navActiveClassName);
         }
     }
-})();
+})($);
